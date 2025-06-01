bc352ef1949f7a913a8479eea9c7ea684ef1a72e
hamazinetwork <hademolahamazin@gmail.com>
2025-06-01 21:03:16 +0100

    specified buttons

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,11 +6,25 @@ const Landing: React.FC =  () => {
     return (
       <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
-       // <Card />
-        // <Button 
-            title="Get Started"
-            style="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full"
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+         <Button 
+            title="Small Rounded-sm"
+            style="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-sm"
         />
+        <Button 
+            title="Medium Rounded-md"
+            style="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-md"
+        />
+        <Button 
+            title="large Rounded-lg"
+            style="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-lg"/>
+            <Button 
+            title="large Rounded-full"
+            style="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full"
+           />
       </div>
     )
   }
